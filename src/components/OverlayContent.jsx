d58d87900acbb949c5bcdd356449ab8a9a9db81a
Huw Roberts <huw@huwroberts.dev>
2025-02-03 22:23:07 +0000

    Fix syntax

--- src/components/OverlayContent.jsx
+++ src/components/OverlayContent.jsx
@@ -2,7 +2,7 @@ export default function OverlayText() {
   return (
     <div className='overlayContent'>
       <div className='tl'>平凡な時代に</div>
-      <div class='br'></div>
+      <div className='br'></div>
       <div className='c'>
         In the age of average
         <br />
